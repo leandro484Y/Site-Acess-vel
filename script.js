6655bcf5e0d97f6e6f5307c99fb4b3f568e12566
leandro484Y <00001115031053SP@al.educacao.sp.gov.br>
2025-08-28 15:12:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,17 @@
  diminuiFonteBotao.addEventListener('click', function () {
  tamanhoAtualFonte -= 0.1;
  document.body.style.fontSize = \${tamanhoAtualFonte}rem``
-
  })
-
+const botaoDeAcessibilidade =
+document.getElementById('botaoacessibilidade');
+ const opcoesDeAcessibilidade =
+document.getElementById('opcoesacessibilidade');
+botaoDeAcessibilidade.addEventListener('click', () => {
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
  })
+ })
+alternaContraste.addEventListener('click', function(){
+ document.body.classList.toggle('alto-contraste');
+ })
+})
